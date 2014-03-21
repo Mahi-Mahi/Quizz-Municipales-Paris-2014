431c423be2026448297ca1ab5fd82619a31c85c5
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-03-21 11:21:11 +0100

    tracking + wording

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,19 +15,23 @@ define(['angular', 'services'], function(angular) {
 				if (!$rootScope.results) {
 					$location.path('/app/');
 				}
+				ga('send', 'event', 'results', 'open');
 				if ($rootScope.results && $rootScope.results.nkm > $rootScope.results.ah) {
 					// NKM
-					$scope.result_title = "Vous partagez plutôt les idées <br />de <strong>Nathalie Kosciusko-Morizet</strong>";
+					$scope.result_title = "Vous partagez plutôt les idées <br />de <strong>Nathalie Kosciusko-Morizet</strong>.";
 					$scope.slug = ['nkm'];
+					ga('send', 'event', 'result', 'nkm');
 				} else {
 					if ($rootScope.results && $rootScope.results.nkm < $rootScope.results.ah) {
 						// AH
-						$scope.result_title = "Vous partagez plutôt les idées <br />de <strong>Anne Hidalgo</strong>";
+						$scope.result_title = "Vous partagez plutôt les idées <br />de <strong>Anne Hidalgo</strong>.";
 						$scope.slug = ['ah'];
+						ga('send', 'event', 'result', 'ah');
 					} else {
 						// draw
-						$scope.result_title = "Vous partagez à égalité les idées <br />des <strong>deux candidates</strong>";
+						$scope.result_title = "Vous partagez à égalité les idées <br />des <strong>deux candidates</strong>.";
 						$scope.slug = ['ah', 'nkm'];
+						ga('send', 'event', 'result', 'ah-nkm');
 					}
 				}
 			}
