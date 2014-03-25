2e38997a02d010e291c96db81328159c109eca32
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-03-25 14:26:02 +0100

    fix

--- app/js/controllers/quizzCtrl.js
+++ app/js/controllers/quizzCtrl.js
@@ -65,7 +65,7 @@ define([], function() {
 			startQuizz();
 		} else {
 			dataService.getData(function(data) {
-				$scope.questions = data.questions.slice(0, 1);
+				$scope.questions = data.questions; //.slice(0, 1);
 				startQuizz();
 			});
 		}
